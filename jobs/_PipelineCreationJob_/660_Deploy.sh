f2dbfa50f0fd7cc3ef17a9cfa7bc50a1373eb4c4
Jim Cornmell <jco@snomed.org>
2023-12-12 13:46:36 +0000

    PIP-476 Upload to docker hub when merged to master.

--- jobs/_PipelineCreationJob_/660_Deploy.sh
+++ jobs/_PipelineCreationJob_/660_Deploy.sh
@@ -2,6 +2,21 @@
 source ../_PipelineCreationJob_/000_Config.sh
 figlet -w 500 "${STAGE_NAME}"
 
+# TODO: https://github.com/docker/docker-credential-helpers#available-programs
+deployToDockerHub() {
+    if [[ $GIT_BRANCH =~ master$ ]] || [[ $GIT_BRANCH =~ main$ ]]; then
+        containsJib=$(xmllint --xpath '/*[local-name()="project"]/*[local-name()="build"]/*[local-name()="plugins"]/*[local-name()="plugin"]/*[local-name()="artifactId"]' pom.xml | grep -c jib-maven-plugin)
+
+        if (( containsJib == 1 )); then
+            figlet -w 500 "Docker Hub"
+            echo "Maven JIB configuration and on $GIT_BRANCH so deploying to DOCKERHUB"
+            echo "$DOCKER_HUB_PSW" | docker login -u "$DOCKER_HUB_USR" --password-stdin
+            mvn jib:build
+            echo "Docker hub deployment completed"
+        fi
+    fi
+}
+
 deployDebianPackages() {
     echo "Uploading to Nexus"
     local rel_type="NONE"
@@ -31,13 +46,13 @@ deployDebianPackages() {
 
                 # See example 7 here: https://www.jenkins.io/doc/book/pipeline/syntax/#environment
                 status=$(curl -o /dev/null --write-out '%{http_code}\n' -u "$NEXUS_LOGIN_USR:$NEXUS_LOGIN_PSW" -X POST -H "Content-Type: multipart/form-data" --data-binary "@${deb_pkg}" "https://nexus3.ihtsdotools.org/repository/${rel_type}/")
-                status=$(curl -o /dev/null --write-out '%{http_code}\n' -u "$NEXUS_LOGIN_USR:$NEXUS_LOGIN_PSW" -X POST -H "Content-Type: multipart/form-data" --data-binary "@${deb_pkg}" "https://nexus3.ihtsdotools.org/repository/${rel_type}/")
                 echo "Status=${status}"
 
                 if (( status > 299 )); then
                     exit 1
                 else
                     echo "Upload successful"
+                    deployToDockerHub
                 fi
             else
                 echo "No debian package found to upload"
