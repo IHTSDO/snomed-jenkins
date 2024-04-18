8c13c7fdab6ceb09732ab706c31a95f1ca307554
Jim Cornmell <jco@snomed.org>
2024-04-18 13:04:14 +0100

    Added clean to start of all builds

--- scripts/010_Initialize.sh
+++ scripts/010_Initialize.sh
@@ -10,9 +10,11 @@ echo "BUILD TOOL: ${SNOMED_PROJECT_BUILD_TOOL}"
 case $SNOMED_PROJECT_BUILD_TOOL in
     maven)
         mvn --version
+        mvn clean
         ;;
     gradle)
         gradle --version
+        gradle clean
         ;;
     none)
         echo "No build tool required."
