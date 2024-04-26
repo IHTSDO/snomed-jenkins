c553beb19283d5b01a152fb136bf016a689757dd
Jim Cornmell <jco@snomed.org>
2024-04-26 13:27:13 +0100

    Initial commit.

---
+++ scripts/650_Audit.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+source "$SCRIPTS_PATH/000_Config.sh"
+figlet -w 500 "Audit"
+
+case $SNOMED_PROJECT_BUILD_TOOL in
+    maven)
+        if [[ "${SNOMED_PROJECT_OWNER,,}" == "snomed" ]]; then
+            mvn \
+                -DskipTests \
+                -Ddependency-check.skip=true \
+                -Dsonar.host.url="${SONAR_URL}" \
+                -Dsonar.token="${SONAR_TOKEN}" \
+                -Dsonar.projectName="${SNOMED_PROJECT_NAME,,}" \
+                sonar:sonar
+        else
+            mvn \
+                -DskipTests \
+                -Ddependency-check.skip=true \
+                -Dsonar.host.url="${SONAR_URL}" \
+                -Dsonar.token="${SONAR_TOKEN}" \
+                -Dsonar.projectName="${SNOMED_PROJECT_NAME,,}" \
+                sonar:sonar -Dsonar.qualitygate.wait=true
+        fi
+        ;;
+    gradle)
+        # https://docs.sonarsource.com/sonarqube/latest/analyzing-source-code/scanners/sonarscanner-for-gradle/
+        gradle sonar \
+            -Dsonar.verbose=true \
+            -Dsonar.host.url="${SONAR_URL}" \
+            -Dsonar.token="${SONAR_TOKEN}"
+        ;;
+    none)
+        echo "No audit tool required."
+        ;;
+    *)
+        echo "Unknown build tool: ${SNOMED_PROJECT_BUILD_TOOL}"
+        exit 1
+        ;;
+esac
