150b18394f6e1d380723711df57ec398c9fc2f3d
Jim Cornmell <jco@snomed.org>
2024-08-07 11:20:07 +0100

    PIP-529 Add support for Typescript projects in audit script.

--- scripts/650_Audit.sh
+++ scripts/650_Audit.sh
@@ -2,28 +2,47 @@
 source "$SCRIPTS_PATH/000_Config.sh"
 figlet -w 500 "Audit"
 
-case $SNOMED_PROJECT_BUILD_TOOL in
-    maven)
-        mvn \
-            -DskipTests \
-            -Ddependency-check.skip=true \
-            -Dsonar.host.url="${SONAR_URL}" \
-            -Dsonar.token="${SONAR_TOKEN}" \
-            -Dsonar.projectName="${SNOMED_PROJECT_NAME,,}" \
-            sonar:sonar -Dsonar.qualitygate.wait=true
-        ;;
-    gradle)
-        # https://docs.sonarsource.com/sonarqube/latest/analyzing-source-code/scanners/sonarscanner-for-gradle/
-        gradle sonar \
-            -Dsonar.verbose=true \
-            -Dsonar.host.url="${SONAR_URL}" \
-            -Dsonar.token="${SONAR_TOKEN}"
-        ;;
-    none)
-        echo "No audit tool required."
+case $SNOMED_PROJECT_LANGUAGE in
+    Typescript)
+        cat>sonar-project.properties<<EOF
+sonar.projectKey=${SNOMED_PROJECT_GROUP_ARTIFACT,,}
+sonar.projectName=${SNOMED_PROJECT_NAME,,}
+sonar.projectVersion=1.0
+sonar.sources=src
+sonar.tests=src
+sonar.language=ts
+sonar.test.inclusions=.
+sonar.exclusions=src/main/**/*,node_modules/**/*,.angular/**/*,dist/**/*
+sonar.sourceEncoding=UTF-8
+EOF
+
+        node_modules/sonarqube-scanner/bin/sonar-scanner -Dsonar.host.url=${SONAR_URL} -Dsonar.login=${SONAR_TOKEN}
         ;;
     *)
-        echo "Unknown build tool: ${SNOMED_PROJECT_BUILD_TOOL}"
-        exit 1
+        case $SNOMED_PROJECT_BUILD_TOOL in
+            maven)
+                mvn \
+                    -DskipTests \
+                    -Ddependency-check.skip=true \
+                    -Dsonar.host.url="${SONAR_URL}" \
+                    -Dsonar.token="${SONAR_TOKEN}" \
+                    -Dsonar.projectName="${SNOMED_PROJECT_NAME,,}" \
+                    sonar:sonar -Dsonar.qualitygate.wait=true
+                ;;
+            gradle)
+                # https://docs.sonarsource.com/sonarqube/latest/analyzing-source-code/scanners/sonarscanner-for-gradle/
+                gradle sonar \
+                    -Dsonar.verbose=true \
+                    -Dsonar.host.url="${SONAR_URL}" \
+                    -Dsonar.token="${SONAR_TOKEN}"
+                ;;
+            none)
+                echo "No audit tool required."
+                ;;
+            *)
+                echo "Unknown build tool: ${SNOMED_PROJECT_BUILD_TOOL}"
+                exit 1
+                ;;
+        esac
         ;;
 esac
