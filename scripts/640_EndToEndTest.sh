59e69c7152074d7a335c822f75b2e09be52a2260
Elena Ilyukhina <eil@ihtsdo.org>
2025-03-24 08:52:09 +0000

    BROWSE-754 Add sct-browser-frontend production job

--- scripts/640_EndToEndTest.sh
+++ scripts/640_EndToEndTest.sh
@@ -16,34 +16,31 @@ if [[ -d cypress ]]; then
             echo "    \"$key\": \"$value\","
         done <<< "$(set | grep "^TEST_" | sed -e 's/=/\t/' -e "s/\t'/\t/" -e "s/'$//" | cut -f 1,2)"
 
-        # Populate config file with URLS depending on branch. main/master = UAT, others = DEV.
-        case $GIT_BRANCH in
-            origin/main | origin/master)
-                if [[ $JOB_NAME =~ ^e2euat ]]; then
-                    echo "    \"URL_LOGIN\": \"https://uat-ims.${SNOMED_TOOLS_URL}/#/login?serviceReferer=\","
-                    echo "    \"URL_BROWSER\": \"https://uat-browser.${SNOMED_TOOLS_URL}\","
-                    echo "    \"URL_AUTHORING\": \"https://uat-authoring.${SNOMED_TOOLS_URL}\","
-                    echo "    \"URL_REPORTING\": \"https://uat-snowstorm.${SNOMED_TOOLS_URL}/reporting/\","
-                    echo "    \"URL_RAD\": \"https://uat-release.${SNOMED_TOOLS_URL}/\","
-                    echo "    \"URL_SIMPLEX\": \"https://uat-simplex.${SNOMED_TOOLS_URL}/\""
-                else
-                    echo "    \"URL_LOGIN\": \"https://uat-ims.${SNOMED_TOOLS_URL}/#/login?serviceReferer=\","
-                    echo "    \"URL_BROWSER\": \"https://uat-browser.${SNOMED_TOOLS_URL}\","
-                    echo "    \"URL_AUTHORING\": \"https://uat-authoring.${SNOMED_TOOLS_URL}\","
-                    echo "    \"URL_REPORTING\": \"https://uat-snowstorm.${SNOMED_TOOLS_URL}/reporting/\","
-                    echo "    \"URL_RAD\": \"https://uat-release.${SNOMED_TOOLS_URL}/\","
-                    echo "    \"URL_SIMPLEX\": \"https://uat-simplex.${SNOMED_TOOLS_URL}/\""
-                fi
-                ;;
-            *)
-                echo "    \"URL_LOGIN\": \"https://dev-ims.${SNOMED_TOOLS_URL}/#/login?serviceReferer=\","
-                echo "    \"URL_BROWSER\": \"https://dev-browser.${SNOMED_TOOLS_URL}\","
-                echo "    \"URL_AUTHORING\": \"https://dev-authoring.${SNOMED_TOOLS_URL}\","
-                echo "    \"URL_REPORTING\": \"https://dev-snowstorm.${SNOMED_TOOLS_URL}/reporting/\","
-                echo "    \"URL_RAD\": \"https://dev-release.${SNOMED_TOOLS_URL}/\","
-                echo "    \"URL_SIMPLEX\": \"https://dev-simplex.${SNOMED_TOOLS_URL}/\""
-                ;;
-        esac
+        # Populate config file with URLS depending on job name.
+        if [[ ${JOB_NAME} =~ ^e2euat.*$ ]]; then
+            echo "    \"URL_LOGIN\": \"https://uat-ims.${SNOMED_TOOLS_URL}/#/login?serviceReferer=\","
+            echo "    \"URL_BROWSER\": \"https://uat-browser.${SNOMED_TOOLS_URL}\","
+            echo "    \"URL_AUTHORING\": \"https://uat-authoring.${SNOMED_TOOLS_URL}\","
+            echo "    \"URL_REPORTING\": \"https://uat-snowstorm.${SNOMED_TOOLS_URL}/reporting/\","
+            echo "    \"URL_RAD\": \"https://uat-release.${SNOMED_TOOLS_URL}/\","
+            echo "    \"URL_SIMPLEX\": \"https://uat-simplex.${SNOMED_TOOLS_URL}/\""
+
+        elif [[ ${JOB_NAME} =~ ^e2eprod.*$ ]]; then
+            echo "    \"URL_LOGIN\": \"https://prod-ims.${SNOMED_TOOLS_URL}/#/login?serviceReferer=\","
+            echo "    \"URL_BROWSER\": \"https://prod-browser.${SNOMED_TOOLS_URL}\","
+            echo "    \"URL_AUTHORING\": \"https://prod-authoring.${SNOMED_TOOLS_URL}\","
+            echo "    \"URL_REPORTING\": \"https://prod-snowstorm.${SNOMED_TOOLS_URL}/reporting/\","
+            echo "    \"URL_RAD\": \"https://prod-release.${SNOMED_TOOLS_URL}/\","
+            echo "    \"URL_SIMPLEX\": \"https://prod-simplex.${SNOMED_TOOLS_URL}/\""
+
+        else
+            echo "    \"URL_LOGIN\": \"https://dev-ims.${SNOMED_TOOLS_URL}/#/login?serviceReferer=\","
+            echo "    \"URL_BROWSER\": \"https://dev-browser.${SNOMED_TOOLS_URL}\","
+            echo "    \"URL_AUTHORING\": \"https://dev-authoring.${SNOMED_TOOLS_URL}\","
+            echo "    \"URL_REPORTING\": \"https://dev-snowstorm.${SNOMED_TOOLS_URL}/reporting/\","
+            echo "    \"URL_RAD\": \"https://dev-release.${SNOMED_TOOLS_URL}/\","
+            echo "    \"URL_SIMPLEX\": \"https://dev-simplex.${SNOMED_TOOLS_URL}/\""
+        fi
 
         echo "}"
     } > "$JSON_FILE"
